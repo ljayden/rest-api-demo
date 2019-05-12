bba3e25c70286b709b47ddc55dd852af38cc454a
Jayden Lee <ljayden330@gmail.com>
2019-05-13 02:20:19 +0900

    "postgreSQL 연동 및 test profile 추가"

--- src/test/java/com/wonana/restapisample/events/EventControllerTest.java
+++ src/test/java/com/wonana/restapisample/events/EventControllerTest.java
@@ -14,6 +14,7 @@ import org.springframework.context.annotation.Import;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -32,6 +33,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @SpringBootTest
 @AutoConfigureMockMvc
 @AutoConfigureRestDocs
+@ActiveProfiles("test")
 @Import(RestDocsConfiguration.class)
 public class EventControllerTest {
 
